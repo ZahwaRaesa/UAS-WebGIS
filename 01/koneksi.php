e60ef1a87cbb86a0a72e169f0825ad0c6a5c2129
Nama Kamu <emailkamu@example.com>
2026-06-12 09:25:07 +0700

    perbaikan database

--- 01/koneksi.php
+++ 01/koneksi.php
@@ -1,7 +1,7 @@
 <?php
-$host = getenv('DB_HOST') ?: 'localhost';
+$host = getenv('DB_HOST') ?: 'db';
 $user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASSWORD') ?: '';
+$pass = getenv('DB_PASSWORD') ?: 'rootpassword';
 $db   = getenv('DB_NAME') ?: 'uas_06';
 
 $conn = @mysqli_connect($host, $user, $pass, $db);
